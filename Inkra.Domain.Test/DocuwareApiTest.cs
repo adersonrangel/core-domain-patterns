65a8db69363d23966cfc9d6d9235ea3f575f0cc7
Aderson Rangel <adersonrangel@gmail.com>
2022-05-04 20:21:18 -0500

    Proyecto de pruebas

---
+++ Inkra.Domain.Test/DocuwareApiTest.cs
@@ -0,0 +1,33 @@
+﻿using Inkra.Parqueadero.Domain;
+using Inkra.Parqueadero.Domain.Generico;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Inkra.Domain.Test
+{
+    public class DocuwareApiTest
+    {
+
+        [Fact]
+        public void Consultar_Docuware_Sucess() 
+        { 
+            DocuwareApi<ModelListado> docuwareApi = new DocuwareApi<ModelListado>();
+            var respuesta = docuwareApi.CONSULTAR_DOCUWARE<ModelListado>(string.Empty, string.Empty, string.Empty);
+
+            Assert.NotNull(respuesta);
+        }
+
+        [Fact]
+        public void Consulta_Docuware_No_Generic() 
+        {
+            DocuwareApi<ModelListado> docuwareApi = new DocuwareApi<ModelListado>();
+            var respuesta = docuwareApi.CONSULTAR_DOCUWARE("MyArchivador", "TipoBusqueda", "ValorBusqueda");
+
+            Assert.NotNull(respuesta);
+        }
+    }
+}
