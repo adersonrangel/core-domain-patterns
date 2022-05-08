89c02aacbf2d30679cf8628d76185de4668ff8cc
Aderson Rangel <adersonrangel@gmail.com>
2022-05-07 19:34:13 -0500

    Creacion de la clase de registro

---
+++ Inkra.Domain.Test/RegisterTest.cs
@@ -0,0 +1,26 @@
+﻿using Inkra.Parqueadero.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Inkra.Domain.Test
+{
+    public class RegisterTest
+    {
+        [Fact]
+        public void Create_Register()
+        {
+            Register register = new();
+
+            register.CheckIn = DateTime.Now;
+            register.CheckOut = DateTime.Now;
+
+            var respuesta = register.In();
+
+            Assert.True(respuesta > 0);
+        }
+    }
+}
---
+++ Inkra.Parqueadero.Domain/Register.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inkra.Parqueadero.Domain
+{
+    public class Register
+    {
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+        public IVehicle Client { get; set; }
+
+        public double In()
+        {
+            return (CheckIn - CheckOut).TotalSeconds;
+        }
+    }
+}
